3c85a8c663792bd0755d8e2fe3474b7b4858a122
lilmagnus <magnus_erlandsen@hotmail.com>
2021-02-25 15:00:53 +0100

    Todo list

--- main.py
+++ main.py
@@ -1,18 +1,1 @@
-class GraphReader():
-
-    def test(self):
-        pass
-
-# First note 
-print('First class')
-
-#Gjorde dette
-print('Im in')
-
-liste = ['en', 'to']
-for x in liste:
-    print(x)
-
-def add(num)
-    num += num
-    return num 
+# Todo : Make graph, make function to read graph, make function to merge small graphs to big
