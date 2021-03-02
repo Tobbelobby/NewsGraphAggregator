b41e0a27d4467de53ad3e3131f2260e895446789
Tobias Kristensen <tkr048@uib.no>
2021-03-02 16:02:50 +0100

    Printer graf

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 # Todo : Make graph, make function to read graph, make function to merge small graphs to big
 
-def test():
-    pass
+from rdflib import Graph, URIRef
+
+g = Graph()
+graph = g.parse(location="knowledge_graph/dataset36.ttl", format="turtle")
+
+print(graph.serialize(format='turtle').decode('utf-8'))
+
+
+
 
 
