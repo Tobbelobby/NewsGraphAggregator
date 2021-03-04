46fec28428d7da18116124c3e73a162db43dae6f
Tobbelobby <tkr048@uib.no>
2021-03-04 11:59:40 +0100

    Jobbing på uib

--- main.py
+++ main.py
@@ -1,12 +1,38 @@
-# Todo : Make graph, make function to read graph, make function to merge small graphs to big
+#Gennerer nøkkler som er unike, lurt å bruker en hash function kanskje
 
-from rdflib import Graph, URIRef
+from rdflib import Graph, XSD
+#import threading
+import time
 
 g = Graph()
-graph = g.parse(location="knowledge_graph/dataset36.ttl", format="turtle")
+graph = g.parse(location="knowledge_graph/Test.ttl", format="turtle")
 
-print(graph.serialize(format='turtle').decode('utf-8'))
+object_list = []
 
+for subject,predicate,object in graph:
+     object_list.append(object)
+
+
+object_list.sort()
+print(object_list[5])
+print(object_list)
+
+
+# for x in object_list:
+#      print(x)
+
+
+
+
+
+
+
+
+#print(graph.serialize(format='turtle').decode('utf-8'))
+
+
+#Dette vil vi ha ut 'https://twitter.com/i/status/1310747766067527686,
+# http://dbpedia.org/resource/Coronavirus, https://www.wikidata.org/entity/Q918
 
 
 
